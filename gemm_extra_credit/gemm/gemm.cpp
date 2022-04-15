f38d10b66e97f8301637edb08192992284399376
kririae <i@yuyuko.cc>
2022-04-15 16:27:14 +0800

    Format and basically finish the blocking version

--- gemm_extra_credit/gemm/gemm.cpp
+++ gemm_extra_credit/gemm/gemm.cpp
@@ -6,23 +6,78 @@
 // Matrix A is M x K
 // Matrix B is K x N
 //
-// Your implementation should make no assumptions about the values contained in any input parameters.
+// Your implementation should make no assumptions about the values contained in
+// any input parameters.
 
-void gemm(int m, int n, int k, double *A, double *B, double *C, double alpha, double beta){
+#include <math.h>
+#include <stdio.h>
 
-    // REPLACE THIS WITH YOUR IMPLEMENTATION
+#define BLOCKSIZE (6)  // 62 * 62(N) * 8(bytes) \approx = 32KBytes
 
-    int i, j, kk;
-    for (i=0; i<m; i++){
-        for (j=0; j<n; j++){
-	    double inner_prod = 0;
-	    for (kk=0; kk<k; kk++){
-	        inner_prod += A[i*k+kk] * B[kk*n+j];
-	    }
-	    C[i*n+j] = alpha * inner_prod + beta * C[i*n+j];
-	}
+void __gemm_naive(int m, int n, int k, double *A, double *B, double *C,
+                   double alpha, double beta);
+void __gemm_blocking(int m, int n, int k, double *A, double *B, double *C,
+                   double alpha, double beta);
+
+void gemm(int m, int n, int k, double *A, double *B, double *C,
+                double alpha, double beta) {
+  // __gemm_naive(m, n, k, A, B, C, alpha, beta);
+  __gemm_blocking(m, n, k, A, B, C, alpha, beta);
+}
+
+inline static void __gemm_block_L1_mul(int si, int sj, int sk, int m, int n,
+                                     int k, double *A, double *B, double *C,
+                                     double alpha, double beta) {
+  int ei = fmin(si + BLOCKSIZE, m);
+  int ej = fmin(sj + BLOCKSIZE, n);
+  int ek = fmin(sk + BLOCKSIZE, k);
+  // int ei = si + BLOCKSIZE;
+  // int ej = sj + BLOCKSIZE;
+  // int ek = sk + BLOCKSIZE;
+  int i, j, kk;
+
+  for (i = si; i < ei; i++) {
+    for (j = sj; j < ej; j++) {
+      double inner_prod = 0;
+      for (kk = sk; kk < ek; kk++)
+        inner_prod += A[i * k + kk] * B[kk * n + j];
+      C[i * n + j] += alpha * inner_prod;
     }
-    
-    // END OF NAIVE IMPLEMENTATION
+  }
+}
+
+void __gemm_blocking(int m, int n, int k, double *A, double *B, double *C,
+                   double alpha, double beta) {
+  int i, j, kk;
+  static_assert(BLOCKSIZE % 2 == 0, "BLOCKSIZE % 2 must be 0");
+  // printf("running gemm_blocking: (m=%d, n=%d, k=%d)\n", m, n, k);
+
+  for (i = 0; i < m; ++i) {
+    for (j = 0; j < n; ++j) {
+      C[i * n + j] *= beta;
+    }
+  }
+
+  for (i = 0; i < m; i += BLOCKSIZE) {
+    for (j = 0; j < n; j += BLOCKSIZE) {
+      for (kk = 0; kk < k; kk += BLOCKSIZE) {
+        __gemm_block_L1_mul(i, j, kk, m, n, k, A, B, C, alpha, beta);
+      }
+    }
+  }
+}
+
+void __gemm_naive(int m, int n, int k, double *A, double *B, double *C,
+                double alpha, double beta) {
+  int i, j, kk;
+  // printf("running gemm_naive: (m=%d, n=%d, k=%d)\n", m, n, k);
+  for (i = 0; i < m; i++) {
+    for (j = 0; j < n; j++) {
+      double inner_prod = 0;
+      for (kk = 0; kk < k; kk++)
+        inner_prod += A[i * k + kk] * B[kk * n + j];
+      C[i * n + j] = alpha * inner_prod + beta * C[i * n + j];
+    }
+  }
 }
 
---
+++ gemm_extra_credit/gemm/report.md
@@ -0,0 +1,35 @@
+# Performance Measurement
+
+without any optimization(calling `__gemm_naive`):
+
+```
+[Intel MKL]:            [5.673] ms      [5.509] GB/s    [378.56] GFLOPS
+[Student GEMM]:         [4201.226] ms   [0.007] GB/s    [0.51] GFLOPS
+[Ref ISPC GEMM]:        [29.492] ms     [1.060] GB/s    [72.82] GFLOPS
+Total squared error student sol: 0.000000
+Total squared error ref ispc: 0.000000
+```
+
+with blocking(calling `__gemm_blocking`), with `BLOCKSIZE=8`:
+
+```
+[Intel MKL]:            [5.604] ms      [5.576] GB/s    [383.18] GFLOPS
+[Student GEMM]:         [775.963] ms    [0.040] GB/s    [2.77] GFLOPS
+[Ref ISPC GEMM]:        [30.361] ms     [1.029] GB/s    [70.73] GFLOPS
+```
+
+however, if we experiment with different blocksize:
+
+| BLOCKSIZE | GFLOPS |
+| 2         | 1.08   |
+| 4         | 2.58   |
+| 6         | 3.13   |
+| 8         | 2.78   |
+| 16        | 1.58   |
+| 32        | 0.86   |
+
+We see that when `BLOCKSIZE=6`, it reaches its maximum speedup.
+And the speedup is 6.13.
+
+I'll investigate its vectorization rate in vtune on my friend's intel machine.
+But yet there's much space for improvement.
